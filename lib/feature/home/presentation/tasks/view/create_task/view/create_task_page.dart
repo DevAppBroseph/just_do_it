e0647cb60ee5625f2113721812b59b1dd4a9af70
Hashishvilli <bochko1307@mail.ru>
2023-04-19 12:03:34 +0300

    add check field empty

--- lib/feature/home/presentation/tasks/view/create_task/view/create_task_page.dart
+++ lib/feature/home/presentation/tasks/view/create_task/view/create_task_page.dart
@@ -4,10 +4,12 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:just_do_it/constants/constants.dart';
+import 'package:just_do_it/core/utils/toasts.dart';
 import 'package:just_do_it/feature/auth/widget/widgets.dart';
 import 'package:just_do_it/feature/home/data/bloc/profile_bloc.dart';
 import 'package:just_do_it/feature/home/presentation/tasks/view/create_task/widgets/category.dart';
@@ -219,41 +221,96 @@ class _CeateTasksState extends State<CeateTasks> {
                   child: CustomButton(
                     onTap: () async {
                       if (page == 1) {
-                        Task newTask = Task(
-                          name: titleController.text,
-                          description: aboutController.text,
-                          subcategory: selectSubCategory!,
-                          dateStart: DateFormat('yyyy-MM-dd').format(startDate),
-                          dateEnd: DateFormat('yyyy-MM-dd').format(endDate),
-                          priceFrom: int.parse(
-                            coastMinController.text.isEmpty
-                                ? '0'
-                                : coastMinController.text,
-                          ),
-                          priceTo: int.parse(
-                            coastMaxController.text.isEmpty
-                                ? '0'
-                                : coastMaxController.text,
-                          ),
-                          region: region ?? '',
-                          file: null,
-                          icon: '',
-                          task: '',
-                          typeLocation: '',
-                          whenStart: '',
-                          coast: '',
-                        );
-                        final profileBloc =
-                            BlocProvider.of<ProfileBloc>(context);
-                        bool res = await Repository()
-                            .createTask(profileBloc.access!, newTask);
-                        if (res) Navigator.of(context).pop();
+                        String error = 'Укажите:';
+                        bool errorsFlag = false;
+
+                        if (startDate == null) {
+                          error += '\n- дату начала';
+                          errorsFlag = true;
+                        }
+                        if (startDate == null) {
+                          error += '\n- дату завершения';
+                          errorsFlag = true;
+                        }
+                        if (coastMinController.text.isEmpty) {
+                          error += '\n- минимальную цену';
+                          errorsFlag = true;
+                        }
+                        if (coastMaxController.text.isEmpty) {
+                          error += '\n- максимальную цену';
+                          errorsFlag = true;
+                        } if (region == null || region!.isEmpty) {
+                          error += '\n- регион';
+                          errorsFlag = true;
+                        } 
+
+                        if (errorsFlag) {
+                          showAlertToast(error);
+                        } else {
+                          showLoaderWrapper(context);
+
+                          Task newTask = Task(
+                            name: titleController.text,
+                            description: aboutController.text,
+                            subcategory: selectSubCategory!,
+                            dateStart:
+                                DateFormat('yyyy-MM-dd').format(startDate),
+                            dateEnd: DateFormat('yyyy-MM-dd').format(endDate),
+                            priceFrom: int.parse(
+                              coastMinController.text.isEmpty
+                                  ? '0'
+                                  : coastMinController.text,
+                            ),
+                            priceTo: int.parse(
+                              coastMaxController.text.isEmpty
+                                  ? '0'
+                                  : coastMaxController.text,
+                            ),
+                            region: region ?? '',
+                            file: null,
+                            icon: '',
+                            task: '',
+                            typeLocation: '',
+                            whenStart: '',
+                            coast: '',
+                          );
+                          final profileBloc =
+                              BlocProvider.of<ProfileBloc>(context);
+                          bool res = await Repository()
+                              .createTask(profileBloc.access!, newTask);
+                          if (res) Navigator.of(context).pop();
+                          Loader.hide();
+                        }
                       } else {
-                        pageController.animateToPage(
-                          1,
-                          duration: const Duration(milliseconds: 600),
-                          curve: Curves.easeInOut,
-                        );
+                        String error = 'Укажите:';
+                        bool errorsFlag = false;
+
+                        if (selectCategory == null) {
+                          error += '\n- категорию';
+                          errorsFlag = true;
+                        }
+                        if (selectSubCategory == null) {
+                          error += '\n- подкатегорию';
+                          errorsFlag = true;
+                        }
+                        if (titleController.text.isEmpty) {
+                          error += '\n- название';
+                          errorsFlag = true;
+                        }
+                        if (aboutController.text.isEmpty) {
+                          error += '\n- описание';
+                          errorsFlag = true;
+                        }
+
+                        if (errorsFlag) {
+                          showAlertToast(error);
+                        } else {
+                          pageController.animateToPage(
+                            1,
+                            duration: const Duration(milliseconds: 600),
+                            curve: Curves.easeInOut,
+                          );
+                        }
                       }
                     },
                     btnColor: ColorStyles.yellowFFD70A,
