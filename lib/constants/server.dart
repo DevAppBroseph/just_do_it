f4dd5cd710ef9db0ffc6f9b2880d280abac3c114
manyar1 <danielars@mail.ru>
2023-05-09 02:06:55 +0300

    server change

--- lib/constants/server.dart
+++ lib/constants/server.dart
@@ -1,2 +1,2 @@
-const String server = 'http://158.160.44.207:9001';
-const String webSocket = '158.160.44.207:9001';
+const String server = 'http://95.163.243.116';
+const String webSocket = '95.163.243.116:9090';
