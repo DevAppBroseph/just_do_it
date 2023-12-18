1dd1b743a179a4981780304579842bb21d423656
Farhat Baymuradov <farhatbaymuradov3@gmail.com>
2023-12-18 16:29:40 +0500

    base url updated to https

--- lib/constants/server.dart
+++ lib/constants/server.dart
@@ -1,3 +1,5 @@
-const String server = 'http://95.142.45.4';
-// const String server = 'https://app.jobyfine.me';
-const String webSocket = '95.142.45.4';
+// const String server = 'http://95.142.45.4';
+// const String webSocket = '95.142.45.4';
+
+const String server = 'https://app.jobyfine.me';
+const String webSocket = 'app.jobyfine.me';
