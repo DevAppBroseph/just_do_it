8718d50615663776361ce266155062169e5098aa
Altynbek Abdiramanov <79136753+altoon-dev@users.noreply.github.com>
2024-06-06 17:54:51 +0500

    aa

--- lib/constants/server.dart
+++ lib/constants/server.dart
@@ -1,7 +1,7 @@
 // const String server = 'http://95.142.45.4';
 // const String webSocket = '95.142.45.4';
 
-const appVersion = '1.6.5';
+const appVersion = '1.6.6';
 
 const String server = 'http://app.jobyfine.me';
 const String webSocket = 'app.jobyfine.me';
